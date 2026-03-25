54c3784cb2124d0d0c801607bbf928766dacbd6f
Lucía <luci.godu@gmail.com>
2026-03-25 16:32:09 +0100

    Tarea de R

---
+++ 18 - 03. 2.R
@@ -0,0 +1,59 @@
+x <- 5
+class(x)
+is.numerci(x)
+is.character(x)
+is.double(x)
+is.integer(x)
+is.logical(x)
+is.na(x)
+is.null(x)
+as.numeric(x)
+
+i <- "pepe"
+is.character(i)
+
+x <- 6
+y <- 2
+
+x+y
+x-y
+x*y
+x/y
+x%%y
+x!=y
+x<=y
+x>=y
+
+pi
+sqrt(x)
+abs(x)
+rm(x)
+
+x <- 5
+
+abs(x)
+rm (x)
+x <- 5.8891
+round (x,2)
+m <- round (x,2)
+
+c <- "hola"
+nchar(c)
+z <- "mundo"
+paste (c,z)
+paste (c, z, sep="-")
+substr(c, start=2, stop=3)
+c <- "HOLA"
+tolower(c)
+toupper(c)
+
+rm(x)
+rm(y)
+
+x <- TRUE
+y <- FALSE
+x == y 
+x < y 
+x > y 
+x & y 
+x | y
